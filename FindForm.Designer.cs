3f1bda4218edcde8bfea6e103119c8eeb473e557
Svetlana Artemova <artemova-1990@ya.ru>
2020-12-30 15:42:48 +0300

    Saving settings added

---
+++ FindForm.Designer.cs
@@ -0,0 +1,107 @@
+﻿
+namespace Find
+{
+    partial class FindForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonFind = new System.Windows.Forms.Button();
+            this.textBoxDirectory = new System.Windows.Forms.TextBox();
+            this.labelDirectory = new System.Windows.Forms.Label();
+            this.textBoxPattern = new System.Windows.Forms.TextBox();
+            this.labelPattern = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonFind
+            // 
+            this.buttonFind.Location = new System.Drawing.Point(721, 13);
+            this.buttonFind.Name = "buttonFind";
+            this.buttonFind.Size = new System.Drawing.Size(75, 23);
+            this.buttonFind.TabIndex = 0;
+            this.buttonFind.Text = "Поиск";
+            this.buttonFind.UseVisualStyleBackColor = true;
+            this.buttonFind.Click += new System.EventHandler(this.findBtn_Click);
+            // 
+            // textBoxDirectory
+            // 
+            this.textBoxDirectory.Location = new System.Drawing.Point(51, 14);
+            this.textBoxDirectory.Name = "textBoxDirectory";
+            this.textBoxDirectory.Size = new System.Drawing.Size(289, 23);
+            this.textBoxDirectory.TabIndex = 1;
+            // 
+            // labelDirectory
+            // 
+            this.labelDirectory.AutoSize = true;
+            this.labelDirectory.Location = new System.Drawing.Point(12, 17);
+            this.labelDirectory.Name = "labelDirectory";
+            this.labelDirectory.Size = new System.Drawing.Size(33, 15);
+            this.labelDirectory.TabIndex = 2;
+            this.labelDirectory.Text = "Путь";
+            // 
+            // textBoxPattern
+            // 
+            this.textBoxPattern.Location = new System.Drawing.Point(426, 14);
+            this.textBoxPattern.Name = "textBoxPattern";
+            this.textBoxPattern.Size = new System.Drawing.Size(289, 23);
+            this.textBoxPattern.TabIndex = 3;
+            // 
+            // labelPattern
+            // 
+            this.labelPattern.AutoSize = true;
+            this.labelPattern.Location = new System.Drawing.Point(356, 17);
+            this.labelPattern.Name = "labelPattern";
+            this.labelPattern.Size = new System.Drawing.Size(52, 15);
+            this.labelPattern.TabIndex = 4;
+            this.labelPattern.Text = "Шаблон";
+            // 
+            // FindForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(808, 450);
+            this.Controls.Add(this.labelPattern);
+            this.Controls.Add(this.textBoxPattern);
+            this.Controls.Add(this.labelDirectory);
+            this.Controls.Add(this.textBoxDirectory);
+            this.Controls.Add(this.buttonFind);
+            this.Name = "FindForm";
+            this.Text = "Поиск файла";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonFind;
+        private System.Windows.Forms.TextBox textBoxDirectory;
+        private System.Windows.Forms.Label labelDirectory;
+        private System.Windows.Forms.TextBox textBoxPattern;
+        private System.Windows.Forms.Label labelPattern;
+    }
+}
+
---
+++ FindForm.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace Find
+{
+    public partial class FindForm : Form
+    {
+        string settingsDir = Environment.ExpandEnvironmentVariables("%AppData%\\FindSettings.txt");
+        string directory = "";
+        string pattern = "";
+        public FindForm()
+        {
+            InitializeComponent();
+            ReadInFile();
+            textBoxDirectory.Text = directory;
+            textBoxPattern.Text = pattern;
+        }
+
+        private void findBtn_Click(object sender, EventArgs e)
+        {
+
+            if (CheckDirectory(textBoxDirectory.Text))
+                directory = textBoxDirectory.Text;
+
+            pattern = textBoxPattern.Text;
+
+            WriteInFile(directory, pattern);
+        }
+        public void WriteInFile(string dir, string pattern)
+        {
+            using (StreamWriter file = new StreamWriter(settingsDir, false))
+            {
+                file.WriteLine(dir);
+                file.WriteLine(pattern);
+            }
+        }
+        public void ReadInFile()
+        {
+            try
+            {
+                if (File.Exists(settingsDir))
+                {
+                    using (StreamReader sr = new StreamReader(settingsDir))
+                    {
+                        directory = sr.ReadLine();
+                        pattern = sr.ReadLine();
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show($"Ошибка чтения файла настроек {e.ToString()}");
+            }
+        }
+        public bool CheckDirectory(string dir)
+        {
+            if (String.IsNullOrEmpty(dir))
+            {
+                MessageBox.Show("Путь не должен быть пустым");
+                return false;
+            }
+
+            if (Directory.Exists(dir))
+            {
+                return true;
+            }
+            else
+            {
+                MessageBox.Show("Путь не найден");
+                return false;
+            }
+        }
+    }
+}
